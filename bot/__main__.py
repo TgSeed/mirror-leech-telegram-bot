412b62db21ec1f3c39c71a3e48646d58e83ec5a5
TgSeed <92082995+TgSeed@users.noreply.github.com>
2024-03-16 09:09:52 +0000

    feat: rclone watchdog

--- bot/__main__.py
+++ bot/__main__.py
@@ -33,7 +33,7 @@ from .helper.ext_utils.jdownloader_booter import jdownloader
 from .helper.ext_utils.status_utils import get_readable_file_size, get_readable_time
 from .helper.ext_utils.telegraph_helper import telegraph
 from .helper.listeners.aria2_listener import start_aria2_listener
-from .helper.mirror_leech_utils.rclone_utils.serve import rclone_serve_booter
+from .helper.mirror_leech_utils.rclone_utils.serve import rclone_serve_booter, rclone_watchdog
 from .helper.telegram_helper.bot_commands import BotCommands
 from .helper.telegram_helper.button_build import ButtonMaker
 from .helper.telegram_helper.filters import CustomFilters
@@ -247,6 +247,7 @@ async def main():
         telegraph.create_account(),
         rclone_serve_booter(),
         sync_to_async(start_aria2_listener, wait=False),
+        rclone_watchdog(),
     )
     create_help_buttons()
 
--- bot/helper/mirror_leech_utils/rclone_utils/serve.py
+++ bot/helper/mirror_leech_utils/rclone_utils/serve.py
@@ -1,12 +1,38 @@
+import threading
 from aiofiles import open as aiopen
 from aiofiles.os import path as aiopath
-from asyncio import create_subprocess_exec
+from asyncio import create_subprocess_exec, sleep, get_event_loop, ensure_future
 from configparser import ConfigParser
 import os
+import httpx
+from logging import getLogger
 
 from bot import config_dict
 
 RcloneServe = []
+
+LOGGER = getLogger(__name__)
+
+
+async def rclone_watchdog():
+    loop = get_event_loop()
+    loop.call_later(60, lambda: ensure_future(rclone_watchdog()))
+    if len(RcloneServe) == 0:
+        return
+    async with httpx.AsyncClient(verify=False) as client:
+        try:
+            await sleep(60.0)
+            r = await client.get(
+                url=f"http://localhost:{config_dict['RCLONE_SERVE_PORT']}", follow_redirects=True, timeout=20.0
+            )
+            if not ((r.status_code >= 200 and r.status_code < 400) or r.status_code != 404):
+                LOGGER.error(f"Rclone WatchDog: non-successful response from rclone serve: {r.status_code}")
+                await rclone_serve_booter()
+        except httpx.RequestError as exc:
+            LOGGER.error(f"Rclone WatchDog: An error occurred while requesting {exc.request.url!r}.")
+            await rclone_serve_booter()
+
+    return
 
 
 async def rclone_serve_booter():
@@ -59,7 +85,7 @@ async def rclone_serve_booter():
         "rlogserve.txt"
     ]
     if (user := config_dict["RCLONE_SERVE_USER"]) and (
-        pswd := config_dict["RCLONE_SERVE_PASS"]
+            pswd := config_dict["RCLONE_SERVE_PASS"]
     ):
         cmd.extend(("--user", user, "--pass", pswd))
     rcs = await create_subprocess_exec(*cmd)
